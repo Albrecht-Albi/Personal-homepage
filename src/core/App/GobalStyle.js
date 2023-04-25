8b612612ec53c44135a7b0b18436eb36ff29ab97
Albrecht-Albi <albrecht.bar1@gmail.com>
2023-04-25 14:12:51 +0200

    format code

--- src/core/App/GobalStyle.js
+++ src/core/App/GobalStyle.js
@@ -12,9 +12,6 @@ html {
 body {
     font-family: 'Inter', sans-serif;
     font-style: normal;
-    transition: background 0.3s;
-    background: ${({ theme }) => theme.colors.site.background};
-    color: ${({ theme }) => theme.colors.site.text};
     padding-bottom: 108px;
     word-break: break-word;
     letter-spacing: 0.05em;
--- src/features/PersonalHomepage/PersonalHomepage/Footer/SocialMedia/index.js
+++ src/features/PersonalHomepage/PersonalHomepage/Footer/SocialMedia/index.js
@@ -2,17 +2,17 @@ import { List, Item, Link } from "./styled"
 import { socialIcons } from "./socialIcons"
 
 const SocialMedia = () => {
-  return (
-    <List>
-      {socialIcons.map(({ name, url, Icon }) => (
-        <Item key={name}>
-          <Link href={url} title={name} target="_blank" rel="noreferrer">
-            <Icon />
-          </Link>
-        </Item>
-      ))}
-    </List>
-  )
+    return (
+        <List>
+            {socialIcons.map(({ name, url, Icon }) => (
+                <Item key={name}>
+                    <Link href={url} title={name} target="_blank" rel="noreferrer">
+                        <Icon />
+                    </Link>
+                </Item>
+            ))}
+        </List>
+    )
 };
 
 export default SocialMedia;
